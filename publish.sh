a109964b17f475913d236ce830d3b8fdd7bef5b6
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-06-05 23:23:50 -0400

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 pushd src
-jekyll build
+bundle exec jekyll build
 popd
 
 git checkout master
 git ls-files | grep -ve '.gitignore\|CNAME' | xargs rm
 cp -r _site/* .
+git add .
 git commit -am "Update site"
 git push
 git checkout src
